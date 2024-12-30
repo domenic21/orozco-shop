8e023afb1ddb924204eb5eaf493d912123da009a
domenic21 <domenicarias21@gmail.com>
2024-12-29 22:17:19 -0500

    adding phone numbers

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,8 +18,20 @@ export default function Footer() {
                 <li className="flex items-center">
                   <Phone className="w-5 h-5 mr-2" />
                   <span>732-713-3607</span>
+            
+                  
+                </li>
+                <li className="flex items-center">
+                  <Phone className="w-5 h-5 mr-2" />
                   <span>732-713-7765</span>
+         
+            
+                  
+                </li>
+                <li className="flex items-center">
+                  <Phone className="w-5 h-5 mr-2" />
                   <span>732--259-4200</span>
+            
                   
                 </li>
                 <li className="flex items-center">
