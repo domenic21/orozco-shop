450f7516e9cb1cabcd7e427a4268f6b1ac092fa4
domenic21 <domenicarias21@gmail.com>
2025-04-01 02:07:17 -0400

    privacy policy link fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -105,9 +105,7 @@ export default function Footer() {
             <p className="text-gray-400 text-sm">
               <Link href="/privacy-policy" className="text-gray-300 hover:text-white transition-colors">Privacy Policy</Link>
             </p>
-            <p className="text-gray-400 text-sm">
-              <Link href="/sitemap" className="text-gray-300 hover:text-white transition-colors">Privacy Policy</Link>
-            </p>
+
             <p className="text-gray-400 text-sm">
               Developed by <a href="https://dominioncodesolutions.com" className="text-gray-300 hover:text-white transition-colors">Dominion Code Solutions</a>
             </p>
